9b9a73960a50ff7b3793327a713f5ce8e0c48eac
Hemmie001 <emmanuelakej@gmail.com>
2024-11-07 12:24:26 +0000

    0

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,18 +1,16 @@
 #!/usr/bin/env python3
-"""
-Flask app
-"""
+"""This is a Basic Flask app"""
+
 from flask import Flask, render_template
 
-# Create a Flask application instance
 app = Flask(__name__)
 
-# Define a route for the root URL ("/")
-@app.route("/")   
 
+@app.route('/')
 def index():
-    return render_template("0-index.html")
+    """Routes to index(default)"""
+    return render_template("0-index.html",)
 
-# Run the development server (optional for testing)
+
 if __name__ == "__main__":
     app.run(debug=True)
