604b28f4b0cdb0d42ab64cea10ec97de3ae54ae5
Hemmie001 <emmanuelakej@gmail.com>
2024-11-07 10:20:53 +0000

    0

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,20 +1,15 @@
 #!/usr/bin/env python3
-"""
-Flask app
-"""
+"""Basic Flask app"""
 from flask import Flask, render_template
-
 
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
-def index() -> str:
-    """
-    Handles / route
-    """
-    return render_template('0-index.html')
+@app.route("/")
+def home():
+    """Home page route"""
+    return render_template("0-index.html")
 
 
 if __name__ == "__main__":
-    app.run(port="5000", host="0.0.0.0", debug=True)
+    app.run(debug=True)
